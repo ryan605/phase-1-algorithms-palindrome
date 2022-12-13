5384e8a015f732eafa5d92662abf513e4495eed0
ryan605 <langatryan26@gmail.com>
2022-12-13 21:28:03 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var reversed = word.split("").reverse().join("")
+
+  if(reversed==word){
+    return true;
+  }else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  input reversed
+  if reversed string === to input 
+  return true
+   else 
+   false
 */
 
 /*
   Add written explanation of your solution here
+  returs true if string is a palindrome and false if not
+  a pilindrome is a word that even when reversed will be same as original word.
 */
 
 // You can run `node index.js` to view these console logs
